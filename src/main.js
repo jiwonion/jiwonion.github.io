a7cad2dc6d7b1b6002cfbf0dd1c29253888e8e57
Jiwon <wlfhddl23@gmail.com>
2023-06-18 00:57:15 +0900

    JS-Header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+//Header를 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
 
-addEventListener('scroll', (e) => {
-  if (window.scrollY > header.clientHeight) {
-    header.style.backgroundColor = 'var(--color-primary)';
-  } else if (window.scrollY <= header.clientHeight) {
-    header.style.backgroundColor = 'transparent';
+document.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight) {
+    //header.style.backgroundColor = 'var(--color-primary)';
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
   }
 });
