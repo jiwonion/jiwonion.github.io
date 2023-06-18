11c7005d6cc525a479029489a9e81a4a4f89825a
Jiwon <wlfhddl23@gmail.com>
2023-06-19 00:57:56 +0900

    JS-Home-Challenge

--- src/main.js
+++ src/main.js
@@ -9,4 +9,27 @@ document.addEventListener('scroll', () => {
   } else {
     header.classList.remove('header--dark');
   }
+
+  if (scrollY > window.scrollY) {
+    homeElement.style.opacity = parseFloat(homeOpacity) + parseFloat(0.01);
+  } else {
+    homeElement.style.opacity = parseFloat(homeOpacity) - parseFloat(0.01);
+  }
+
+  homeOpacity = window
+    .getComputedStyle(homeElement)
+    .getPropertyValue('opacity');
+  scrollY = window.scrollY;
 });
+
+//Home 섹션을 아래로 스크롤시 투명하게 처리
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', () => {});
+
+const about = document.querySelector('#about');
+const aboutOffsetTop = about.offsetTop;
+
+let scrollY = window.scrollY;
+let homeOpacity = 1;
